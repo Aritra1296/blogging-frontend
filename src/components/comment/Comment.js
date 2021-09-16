99bc3a91e3ea8c3a99663b66f28f2c23c0d4189c
Aritra1296 <aritrivu@gmail.com>
2021-09-16 09:22:42 +0530

    comment timestamp added

--- src/components/comment/Comment.js
+++ src/components/comment/Comment.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import './Comment.css'
+import moment from 'moment'
 
 const Comment = ({ comment }) => {
   return (
     <div>
       <h1>{comment.userName}</h1>
       <h1>{comment.comment}</h1>
+      <h1>{moment(comment.timestamp).format('llll')}</h1>
     </div>
   )
 }
