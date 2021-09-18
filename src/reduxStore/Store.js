0107c5a7248ad725aed99e8c2d7a1011b688a85b
Aritra1296 <aritrivu@gmail.com>
2021-09-18 17:17:42 +0530

    redux update

--- src/reduxStore/Store.js
+++ src/reduxStore/Store.js
@@ -1,12 +1,13 @@
-import { createStore, applyMiddleware } from 'redux'
-import userReducer from '../reducer/Reducer'
-import ReduxThunk from 'redux-thunk'
-import { composeWithDevTools } from 'redux-devtools-extension'
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import * as reducers from '../reducer/Reducer'
+import thunk from 'redux-thunk'
 
+const reducer = combineReducers(reducers)
 
-const store = createStore(userReducer, composeWithDevTools(
-  //applyMiddleware(...middleware),
-  // other store enhancers if any
-));
+const store = createStore(
+  reducer,
+  //applyMiddleware(thunk),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 export default store
